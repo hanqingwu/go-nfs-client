d87eb76bc665eeeaa2d6ff8c354369dd3eede37b
wuhanqing <wuhanqing@ruijie.com.cn>
2022-04-02 10:42:55 +0800

    增加支持hardlink

--- nfs4/client.go
+++ nfs4/client.go
@@ -7,13 +7,14 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"fmt"
-	. "github.com/Cyberax/go-nfs-client/internal"
 	"io"
 	"math"
 	"net"
 	"os"
 	"strings"
 	"time"
+
+	. "github.com/Cyberax/go-nfs-client/internal"
 )
 
 const NfsReadBlockLen = 512 * 1024
@@ -29,6 +30,7 @@ type NfsInterface interface {
 
 	GetFileList(path string) ([]FileInfo, error)
 	GetFileInfo(path string) (FileInfo, error)
+	HardLinkFile(oldpath string, newpath string, path string) error
 	ReadFileAll(path string, writer io.Writer) (uint64, error)
 	ReadFile(path string, offset, count uint64, writer io.Writer) (uint64, error)
 
@@ -288,6 +290,7 @@ func (c *NfsClient) Ping() error {
 }
 
 func (c *NfsClient) runNfsTransaction(ops []Nfs_argop4, pathHint string) ([]Nfs_resop4, error) {
+
 	compound := XdrProc_NFSPROC4_COMPOUND{}
 
 	args := compound.GetArg().(*COMPOUND4args)
@@ -406,12 +409,12 @@ func (c *NfsClient) GetFileList(path string) ([]FileInfo, error) {
 	dirFh := res[len(res)-2].Opgetfh().Resok4().Object
 
 	curDirList := res[len(res)-1].Opreaddir().Resok4()
-	for ; ; {
+	for {
 		ent := curDirList.Reply.Entries
 		if ent == nil {
 			break
 		}
-		for ; ; {
+		for {
 			fileList = append(fileList, c.translateFileMeta(string(ent.Name), ent.Attrs))
 			if ent.Nextentry == nil {
 				break
@@ -551,7 +554,7 @@ func (c *NfsClient) ReadFile(path string, offset, count uint64, writer io.Writer
 	fileFh := res[len(res)-2].Opgetfh().Resok4().Object
 
 	var dataRead uint64
-	for ; ; {
+	for {
 		_, err := writer.Write(flDataBlock.Data)
 		if err != nil {
 			return 0, err
@@ -712,7 +715,7 @@ func (c *NfsClient) WriteFile(path string, truncate bool, offset uint64,
 	}()
 
 	block := make([]byte, NfsReadBlockLen)
-	for ; ; {
+	for {
 		var curRead int
 		curRead, err = reader.Read(block)
 		if curRead == 0 || err == io.EOF {
@@ -735,7 +738,7 @@ func (c *NfsClient) WriteFile(path string, truncate bool, offset uint64,
 }
 
 func (c *NfsClient) writeBlock(id Stateid4, fh Nfs_fh4, offset uint64, data []byte, path string) error {
-	for ; len(data) > 0; {
+	for len(data) > 0 {
 		res, err := c.runNfsTransaction([]Nfs_argop4{
 			{
 				Argop: OP_PUTFH,
@@ -763,6 +766,38 @@ func (c *NfsClient) writeBlock(id Stateid4, fh Nfs_fh4, offset uint64, data []by
 	return nil
 }
 
+func (c *NfsClient) HardLinkFile(oldpath string, newpath string, path string) error {
+	splits := splitPath(oldpath)
+	// Put the directory FH as the current one
+	args := c.makePathLookupArgs(splits)
+	// Make the file claim (i.e. the file name on top of the directory FH)
+	args = append(args,
+		Nfs_argop4{
+			Argop: OP_SAVEFH,
+		})
+
+	newsplits := splitPath(newpath)
+	argsNew := c.makePathLookupArgs(newsplits[0 : len(newsplits)-1])
+	args = append(args, argsNew...)
+
+	flClaim := Component4(newsplits[len(newsplits)-1])
+
+	args = append(args,
+		Nfs_argop4{
+			Argop: OP_LINK,
+			U: &LINK4args{
+				Newname: flClaim,
+			},
+		})
+
+	_, err := c.runNfsTransaction(args, oldpath)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
 func (c *NfsClient) DeleteFile(path string) error {
 	splits := splitPath(path)
 	// Put the directory FH as the current one
